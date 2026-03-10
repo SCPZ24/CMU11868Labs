55ae5e4e0143a029acb2497aa33c3388b754efb2
zytwd <1845371061@qq.com>
2026-03-10 11:43:41 +0800

    拉取作业4仓库，复制脚手架代码。

---
+++ llmsys_hw4/minitorch/modules_basic.py
@@ -0,0 +1,147 @@
+"""
+For additional transformer related
+
+Sequential
+Embedding
+
+"""
+import numpy as np
+
+from .module import Module, Parameter
+from .tensor_functions import (zeros, ones, rand, tensor, tensor_from_numpy, zeros_tensor_from_numpy, ones_tensor_from_numpy)
+from .nn import one_hot
+from .tensor_ops import TensorBackend
+from .tensor import Tensor
+
+from typing import Any, Dict, Optional, Sequence, Tuple
+
+
+class Embedding(Module):
+    def __init__(self, num_embeddings: int, embedding_dim: int, backend: TensorBackend):
+        super().__init__()
+        """
+        Maps one-hot word vectors from a dictionary of fixed size to embeddings.
+
+        Args:
+            num_embeddings : The vocabulary size
+            embedding_dim : The size of each embedding vector
+
+        Attributes:
+            weight : The learnable weights of shape (num_embeddings, embedding_dim) initialized from N(0, 1).
+        """
+        self.backend = backend
+        self.num_embeddings = num_embeddings # Vocab size
+        self.embedding_dim  = embedding_dim  # Embedding Dimension
+        
+        # COPY FROM ASSIGN2_3
+        self.weights = Parameter((rand((num_embeddings, embedding_dim), backend, True) - 0.5) * 2)
+    
+    def forward(self, x: Tensor):
+        """Maps word indices to one-hot vectors, and projects to embedding vectors.
+
+        Args:
+            x : Tensor of shape (batch_size, seq_len)
+
+        Returns:
+            output : Tensor of shape (batch_size, seq_len, embedding_dim)
+        """
+        batch_size, seq_len = x.shape
+        map = one_hot(x, self.num_embeddings).view(batch_size * seq_len, self.num_embeddings) # (batch, seqlen, num_embed)
+        res = map @ self.weights.value
+        return res.view(batch_size, seq_len, self.embedding_dim)
+
+    
+class Dropout(Module):
+    def __init__(self, p_dropout: float=0.1):
+        super().__init__()
+        """During training, randomly zeroes some of the elements of the input tensor with probability :attr:`p_dropout`.
+
+        Attributes: 
+            p_dropout : Probability an element will be zeroed.
+        """
+        self.p_dropout = p_dropout
+
+    def forward(self, x: Tensor) -> Tensor: 
+        """During training, randomly zero out elements of a tensor and scale by (1 - p_dropout)
+        
+        Args: 
+            x : Tensor of shape (*)
+        
+        Returns: 
+            output : Tensor of shape (*)
+        """
+        if not self.training or self.p_dropout == 0:
+            return x
+        else:
+            mask = tensor_from_numpy(np.random.binomial(1, 1-self.p_dropout, x.shape))
+            return x * mask / (1-self.p_dropout)
+
+
+class Linear(Module):
+    def __init__(self, in_size: int, out_size: int, bias: bool, backend: TensorBackend):
+        super().__init__()
+        """Applies a linear transformation to the incoming data. (Same as PyTorch)
+
+        Parameters:
+            in_size  - The size of the dimension the transformation will be applied to
+            out_size - The size of the resulting transformation's dimension
+            bias     - If True, then add an additive bias
+
+        Attributes:
+            weight - The learnable weights of shape (in_size, out_size) initialized from Uniform(-1/sqrt(1/in_size), 1/sqrt(1/in_size)).
+            bias   - The learnable weights of shape (out_size, ) initialized from Uniform(-1/sqrt(1/in_size), 1/sqrt(1/in_size)).
+        """
+        self.weights = Parameter((rand((in_size, out_size), backend, True) - 0.5) * 2 / np.sqrt(in_size))
+        self.bias = Parameter((rand((out_size,), backend, True) - 0.5) * 2 / np.sqrt(in_size)) if bias else None
+
+    def forward(self, x: Tensor):
+        """Applies a linear transformation to the incoming data.
+        
+        Args: 
+            x : Tensor of shape (n, in_size)
+        
+        Returns:
+            output : Tensor of shape (n, out_size)
+        """
+        if self.bias is None:
+            return x @ self.weights.value
+        else:
+            return x @ self.weights.value + self.bias.value
+
+
+class LayerNorm1d(Module):
+    def __init__(self, dim: int, eps: float, backend: TensorBackend):
+        super().__init__()
+        """Applies Layer Normalization over a mini-batch of 1-dimensional inputs.
+        
+        Args: 
+            dim : Expected size of the last dimension to apply layer normalization.
+            eps : A value added for numerical stability.
+        
+        Attributes: 
+            weights : the learnable weights of the module of shape (self.dim, ) initialized to 1.
+            bias    : the learnable bias of the module of shape (self.dim, ) initialized to 0.
+        """
+        self.dim = dim
+        self.eps = eps
+        
+        self.weights = Parameter(ones_tensor_from_numpy((dim,), backend))
+        self.bias = Parameter(zeros_tensor_from_numpy((dim,), backend))
+
+
+    def forward(self, x: Tensor) -> Tensor:
+        """Applies Layer Normalization over a mini-batch of inputs. 
+        NOTE: You can assume the input to this layer is a 2D tensor of shape (batch_size, dim)
+        You will use implicit broadcasting in miniTorch to use the weight and bias.
+        
+        Input: 
+            x - Tensor of shape (bs, dim)
+        
+        Output: 
+            output - Tensor of shape (bs, dim)
+        """
+        mu = x.mean(dim = 1)
+        sigma = (x.var(dim = 1) + self.eps) ** 0.5
+        moved = x - mu
+        norm = moved / sigma
+        return norm * self.weights.value + self.bias.value
